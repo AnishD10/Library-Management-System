addf1af05b9ff114e1b083c643a28540d610efa9
Anish Dangal <122536008+AnishD10@users.noreply.github.com>
2025-07-20 20:31:42 +0545

    Reminder Email(incomplete)

--- Server/Models/Records.js
+++ Server/Models/Records.js
@@ -5,7 +5,9 @@ const recordSchema = new mongoose.Schema({
   borrowerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Borrower', required: true },
   librarianId: { type: mongoose.Schema.Types.ObjectId, ref: 'Librarian', required: true },
   issueDate: { type: Date, default: Date.now },
-  returnDate: { type: Date, default: () => new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) }, // Default to 30 days from issue
+  dueDate: { type: Date, default: () => new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) },
+  returnDate: {type:Date , default: null }, // Default to null until returned
+  reminderDate : {type:Date , default : () => new Date(Date.now() + 14 * 24 * 60 * 1000) }, // Default to 14 days from issue
   status: { type: String, enum: ['issued', 'returned'], required: true },
 });
 
