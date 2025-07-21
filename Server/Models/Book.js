4e75d45a4adbbdc5d1c3e0974bfc6901f45bdf08
Anish Dangal <122536008+AnishD10@users.noreply.github.com>
2025-07-21 10:31:15 +0545

    Removed reminder

--- Server/Models/Book.js
+++ Server/Models/Book.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
+  isbn : { type: String, required: true, unique: true },
   quantity: { type: Number, required: true },
   available: { type: Number, required: true },
 });
