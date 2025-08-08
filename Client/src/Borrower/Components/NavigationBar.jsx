9303d293d6db395e94e602c09826e2917959090a
AnishD10 <manxekhatra@gmail.com>
2025-08-08 23:56:46 +0545

    Navigation Complete

--- Client/src/Borrower/Components/NavigationBar.jsx
+++ Client/src/Borrower/Components/NavigationBar.jsx
@@ -6,8 +6,8 @@ const NavigationBar = () => {
 
     return (
 <>
-<div className="Nav-Bar">
-<div className="Nav-links ">
+<div className=" h-[100px] w-full bg-[#d9d9d9] flex items-center justify-around">
+<div className="Nav-links h-full w-[500px] flex justify-evenly items-center">
 <Link to="/Home" className="Home">Home</Link>
 <Link to="/AboutUs" className="AboutUs">About US</Link>
 <Link to="/ContactUs" className="ContactUs">Contact Us</Link>
@@ -15,6 +15,7 @@ const NavigationBar = () => {
 <Link to="/Research" className="Research">Research</Link>
 </div>
 <div className="Icons"></div>
+<button className="h-12 w-24 bg-white rounded-full">Login</button>
 </div>
 
 </>
